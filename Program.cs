42ed2d611202fb1fc4eaeacf315241b5c6ae92e8
twdeveloper <twdeveloper@outlook.com>
2021-11-08 22:37:55 +0800

    v1

---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Media;
+// Import namespaces
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Text;
+
+namespace speaking_clock
+{
+    class Program
+    {
+        private static SpeechConfig speechConfig;
+        static async Task Main(string[] args)
+        {
+            try
+            {
+
+                // Set console encoding to unicode
+                Console.InputEncoding = Encoding.Unicode;
+                Console.OutputEncoding = Encoding.Unicode;
+
+                // Get config settings from AppSettings
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                string cogSvcKey = configuration["CognitiveServiceKey"];
+                string cogSvcRegion = configuration["CognitiveServiceRegion"];
+
+                // Configure speech service
+                speechConfig = SpeechConfig.FromSubscription(cogSvcKey, cogSvcRegion);
+                Console.WriteLine("Ready to use speech service in " + speechConfig.Region);
+
+
+                // Configure voice
+                speechConfig.SpeechSynthesisVoiceName = "en-US-AriaNeural";
+
+                // Get spoken input
+                string command = "";
+                command = await TranscribeCommand();
+                if (command.ToLower() == "what time is it?")
+                {
+                    await TellTime();
+                }
+
+                await MyMessage();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static async Task<string> TranscribeCommand()
+        {
+            string command = "";
+
+            // Configure speech recognition
+            string audioFile = "time.wav";
+            SoundPlayer wavPlayer = new SoundPlayer(audioFile);
+            wavPlayer.Play();
+            using AudioConfig audioConfig = AudioConfig.FromWavFileInput(audioFile);
+            using SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+
+            // Process speech input
+            SpeechRecognitionResult speech = await speechRecognizer.RecognizeOnceAsync();
+            if (speech.Reason == ResultReason.RecognizedSpeech)
+            {
+                command = speech.Text;
+                Console.WriteLine(command);
+            }
+            else
+            {
+                Console.WriteLine(speech.Reason);
+                if (speech.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(speech);
+                    Console.WriteLine(cancellation.Reason);
+                    Console.WriteLine(cancellation.ErrorDetails);
+                }
+            }
+
+            // Return the command
+            return command;
+        }
+
+        static async Task TellTime()
+        {
+            var now = DateTime.Now;
+            string responseText = "The time is " + now.Hour.ToString() + ":" + now.Minute.ToString("D2");
+
+            // Configure speech synthesis
+            speechConfig.SpeechSynthesisVoiceName = "en-GB-RyanNeural";
+            using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig);
+
+            // Synthesize spoken output
+            SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(responseText);
+            if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+            {
+                Console.WriteLine(speak.Reason);
+            }
+
+
+            // Print the response
+            Console.WriteLine(responseText);
+        }
+
+        static async Task MyMessage()
+        {
+            var now = DateTime.Now;
+            string responseText = "中原標準時間，" + now.Hour.ToString() + ":" + now.Minute.ToString("D2");
+
+            // Configure speech synthesis
+            speechConfig.SpeechSynthesisLanguage = "zh-TW";
+            speechConfig.SpeechSynthesisVoiceName = "zh-TW-HsiaoChenNeural";
+            using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig);
+
+            // Synthesize spoken output
+            SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(responseText);
+            if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+            {
+                Console.WriteLine(speak.Reason);
+            }
+
+
+            // Print the response
+            Console.WriteLine(responseText);
+        }
+
+    }
+}
